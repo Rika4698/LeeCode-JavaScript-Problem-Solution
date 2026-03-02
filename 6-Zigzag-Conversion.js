f6f4244a9db6975216cf9f50066669b327338f9b
Rika4698 <rekasharmin46@gmail.com>
2026-03-03 01:41:38 +0600

    added 6 no

--- 6-Zigzag-Conversion.js
+++ 6-Zigzag-Conversion.js
@@ -11,4 +11,16 @@ var convert = function(s, numRows) {
     let currentRow = 0;
     let goingDown = false;
 
+     for(let char of s){
+        rows[currentRow] = rows[currentRow] + char;
+        if(currentRow === 0 || currentRow === numRows -1){
+            goingDown = !goingDown;
+
+        }
+        currentRow += goingDown ? 1: -1;
+    }
+    return rows.join("");
+
 }
+
+console.log(convert("PAYPALISHIRING", 3));
