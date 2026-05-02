ab04e19b376dea2d0ad86825ca7d5ac336dff0bb
Rika4698 <rekasharmin46@gmail.com>
2026-05-02 23:07:16 +0600

    -- Write your PostgreSQL query statement below

---
+++ Database/184-DepartmentHighestSalary.sql
@@ -0,0 +1,15 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+d.name AS "Department",
+e.name AS "Employee",
+e.salary AS "Salary"
+FROM Employee e
+JOIN (
+    SELECT departmentId, MAX(salary) AS max_salary
+    FROM Employee
+    GROUP BY departmentId
+) m 
+ON e.departmentId = m.departmentId
+AND e.salary = m.max_salary
+JOIN Department d
+ON e.departmentId = D.id;
