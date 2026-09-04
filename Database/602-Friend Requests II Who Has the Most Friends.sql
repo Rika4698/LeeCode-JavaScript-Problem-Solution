0a77d37615f00392c7339eddb4654033c59d1d0f
Rika4698 <rekasharmin46@gmail.com>
2026-09-04 12:00:00 +0600

    Who Has the Most Friends

---
+++ Database/602-Friend Requests II Who Has the Most Friends.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id 
+    FROM RequestAccepted
+
+    UNION ALL
+    SELECT  accepter_id AS id
+    FROM RequestAccepted
+) AS friend
+GROUP BY id 
+ORDER BY num DESC
