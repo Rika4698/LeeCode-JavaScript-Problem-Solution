8462ea0a6d85a23a02f5f8aab479773b16ceddc9
Rika4698 <rekasharmin46@gmail.com>
2026-09-12 00:26:10 +0600

    patients-with-a-condition

--- Database/1527-Patients With a Condition.sql
+++ Database/1527-Patients With a Condition.sql
@@ -2,3 +2,22 @@
 SELECT p.patient_id, p.patient_name, p.conditions
 FROM Patients p
 WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
+
+
+-- LIKE is used for pattern matching.
+
+-- name LIKE 'A%'
+
+-- means:
+
+-- Name starts with A.
+
+-- % means:
+
+-- Any number of characters can come after this.
+-- First %
+-- %
+
+-- means:
+
+-- There can be anything before DIAB1.
