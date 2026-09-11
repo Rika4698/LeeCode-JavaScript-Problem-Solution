233bf6c2770c9a269a4916033015153ac348ce1b
Rika4698 <rekasharmin46@gmail.com>
2026-09-12 00:14:53 +0600

    top-travellers

--- Database/1327-List the Products Ordered in a Period.sql
+++ Database/1327-List the Products Ordered in a Period.sql
@@ -7,3 +7,24 @@ WHERE o.order_date >= DATE '2020-02-01'
 AND o.order_date < DATE '2020-03-01'
 GROUP BY p.product_id, p.product_name
 HAVING SUM(o.unit) >= 100;
+
+
+-- WHERE
+--  ↓
+-- Filter rows
+
+-- GROUP BY
+--  ↓
+-- Create groups
+
+-- SUM()
+--  ↓
+-- Calculate total
+
+-- HAVING
+--  ↓
+-- Filter groups
+-- Easy rule:
+
+-- WHERE → before GROUP BY
+-- HAVING → after GROUP BY
